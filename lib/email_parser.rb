718a4354291cc1be3938d6c7d2f351c00db73e31
Alma Flores <flores.almalee@gmail.com>
2020-08-26 21:41:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
   end
   
   def parse
-    email.split(/[,\s]/).map{|email| email != " "}.uniq!
+    email.split(/[,\s]/).select{|email| email != " "}.uniq!
   end
  
 end
